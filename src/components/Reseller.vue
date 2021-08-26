d2ee1616d91937ea2f2ff1b47f74c726a7aef4c8
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-08-26 11:30:22 +0200

    init resellers
    Ajout de la liste des resellers, d'un component reseller, des datas, import/export

---
+++ src/components/Reseller.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="reseller">
+  <h2>Liste des Revendeurs</h2>
+  <ul>
+    <li>
+      <h3>{{ name }}</h3>
+    </li>
+    <li>{{ description }}</li>
+    <li>Ajouté le : {{ created_at.toLocaleString() }}</li>
+    <li>Mis à jour le : {{ updated_at.toLocaleString() }}</li>
+  </ul>
+</div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
+
+
+<script>
+export default {
+  name: 'Reseller',
+  props: {
+    name: String,
+    description: String,
+    latitude: Number,
+    longitude: Number,
+    created_at: String,
+    updated_at: String
+  }
+}
+</script>
