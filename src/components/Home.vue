c20c0963fd2744180f4d72692624cbbc877b04a5
Lucie GRIVEAU <lucie.griveau@Digital-Grenoble.local>
2021-08-26 13:24:21 +0200

    addition components Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <CustomersList/>
+    <ResellersList/>
+    <SuppliersList/>
   </div>
 </template>
 
 <script>
+import CustomersList from './CustomersList.vue'
+import ResellersList from './ResellersList.vue'
+import SuppliersList from './CustomersList.vue'
+
 export default {
   name: 'Home',
   props: {
     msg: String
+  },
+  components: {
+    CustomersList,
+    ResellersList,
+    SuppliersList,
   }
 }
 </script>
