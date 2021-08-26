861ea06e3474c945266aa71c0114c1cb8eefa2ab
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-08-26 12:02:27 +0200

    test css
    ajout css list

--- src/components/Reseller.vue
+++ src/components/Reseller.vue
@@ -18,18 +18,41 @@ h3 {
   margin: 40px 0 0;
 }
 
+/* List */
 ul {
-  list-style-type: none;
+  counter-increment: index;
   padding: 0;
+  max-width: 300px;
 }
 
+/* List element */
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: flex;
+  align-items: center;
+  padding: 12px 0;
+  box-sizing: border-box;
 }
 
-a {
-  color: #42b983;
+/* Element counter */
+ul::before {
+  content: counters(index, ".", decimal-leading-zero);
+  font-size: 1.5rem;
+  text-align: right;
+  font-weight: bold;
+  min-width: 50px;
+  padding-right: 12px;
+  font-variant-numeric: tabular-nums;
+  align-self: flex-start;
+  background-image: linear-gradient(to bottom, aquamarine, orangered);
+  background-attachment: fixed;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+
+/* Element separation */
+li + li {
+  border-top: 1px solid rgba(255,255,255,0.2);
 }
 </style>
 
