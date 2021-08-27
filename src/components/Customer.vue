dc2874894a494f4c32ac04fc2bda07ead05fa67b
Lucie GRIVEAU <lucie.griveau@Digital-Grenoble.local>
2021-08-27 16:59:54 +0200

    working refreshing page into customer

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -4,20 +4,20 @@
       <div class="card-content">
 
         <div class="media">
-          <div class="media-left">
-            <figure class="customer_picture">
-              <img src="" alt="Placeholder image">
-            </figure>
-          </div>
+          <!--          <div class="media-left">-->
+          <!--            <figure class="customer_picture">-->
+          <!--              <img src="" alt="Placeholder image">-->
+          <!--            </figure>-->
+          <!--          </div>-->
           <div class="media-content">
-            <h1 class="fist_name">{{ customer.firstname + ' ' + customer.lastname }}</h1>
-            <p class="subtitle is-6">Email: {{ customer.email }}</p>
+            <h1 class="fist_name">{{ test.firstName + ' ' + test.lastName }}</h1>
+            <p class="subtitle is-6">Email: {{ test.email }}</p>
           </div>
         </div>
 
         <div class="content">
-          <a>Adresse:{{ customer.address + ' ' + customer.postalCode + ' ' + customer.city }}</a>
-          <p> {{ customer.phoneNumber }}</p>
+          <a>Adresse:{{ test.address + ' ' + test.postalCode + ' ' + test.city }}</a>
+          <p>{{ test.phoneNumber }}</p>
         </div>
 
       </div>
@@ -26,17 +26,44 @@
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: "Customer",
-  props:{
+  props: {
     customer: Object,
   },
-  data(){
+  data() {
     return {
-
+      test: {}
+    }
+  },
+  methods: {
+    getData() {
+      this.loading = true
+      axios
+          .get('https://heroku-campus-suppliers.herokuapp.com/api/customers/' + this.$route.params.id)
+          .then(response => {
+            this.test = response.data;
+            // console.log(response.data.data);
+          })
+          .catch(error => {
+            // console.log(error)
+            this.error = error
+          })
+          .finally(() => {
+            this.loading = false;
+          })
+    }
+  },
+  mounted() {
+    if (this.customer === undefined) {
+      console.log(this.$route.params.id);
+      this.getData()
+    } else {
+      this.test = this.customer
     }
   }
-
 }
 </script>
 
